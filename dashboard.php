64e357121f1d81850be8d3241b63c0697ea57298
fernando <devfernando@gmail.com>
2020-05-14 18:20:32 +0000

    update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -17,11 +17,17 @@ if(isset($_SESSION['user']) && isset($_SESSION['password'])){
 ?>
 	
 	<div class="ed-container">
-		<div class="ed-item s-1-5">
+		<div class="ed-item s-1-2">
 			<h3>Usuario Logueado</h3>
 		</div>
-		<div class="ed-item s-1-5">
+		<div class="ed-item s-1-2">
 			<h2><?php echo $_SESSION['user']; ?></h2>
+		</div>
+		<div class="ed-item">
+			<h4>Peticion procesada por el nodo con ip: <?php echo $_SERVER['SERVER_ADDR']; ?></h4>
+		<div>
+		<div class="ed-item">
+			<h4>En el server balanceador: <?php echo $_SERVER['SERVER_HOST']; ?></h4>
 		</div>
 	</div>
 
