3064d1eb4a9d6d75194d2a4c382a281b1f8f0268
fernando <devfernando@gmail.com>
2020-05-14 17:50:12 +0000

    initial commit

--- header.php
+++ header.php
@@ -6,14 +6,20 @@
 	<link rel="stylesheet" type="text/css" href="/sessionbalancer/assets/css/materialize.css" />
         </head>
 
-        <body>
+<body>
 
-  <nav class="blue">
-    <div class="ed-container nav-wrapper blue">
-      <a href="#" class="brand-logo">Protocolos de comunicacion: server1</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        
+<nav class="blue">
+<div class="ed-container">
+<div class="ed-item">
+    <div class="nav-wrapper blue">
+      <a href="#" class="ed-item brand-logo">PROTOCOLOS DE COMUNICACION</a>
+      <ul id="nav-mobile" class=" right hide-on-med-and-down">
+        <li><a href="" class="flow-text active"><?php echo "NODO " ;?></a></li>
+        <li><a href="" class="flow-text active" ><?php echo "IP  " . $_SERVER['SERVER_ADDR']; ?></a></li>
+        <li><a href="" class="flow-text active"><?php echo "PORT  " . $_SERVER['SERVER_PORT']; ?></a></li>
       </ul>
     </div>
+</div>
+</div>
   </nav>
-  
+
